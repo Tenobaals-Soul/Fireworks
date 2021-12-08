4b3bc89a301b2ebb77b3c89a83e99f2b8da386f7
Tenobaals_Soul <simon.giesel@gmx.net>
2021-12-08 17:03:46 +0100

    firework particles working

--- src/main.c
+++ src/main.c
@@ -31,26 +31,39 @@ typedef struct RGB {
     GLubyte r, g, b;
 } RGB;
 
-typedef struct Particle {
-    VectorFloat position;
-    VectorFloat linear_speed;
-    float direction;
-    float speed;
-    char active;
-} Particle;
-
 typedef struct Trail {
     unsigned long since;
     VectorFloat position;
+    RGB color;
     float angle;
     struct Trail* next;
 } Trail;
 
+typedef struct Particle {
+    VectorFloat position;
+    VectorFloat linear_speed;
+    RGB color;
+    unsigned long die_at;
+    float direction;
+    float speed;
+    Trail* trail;
+    char active;
+} Particle;
+
+#define PS_LERP 1
+#define PS_RAND 2
 typedef struct ParticleSystem {
     Particle* current;
     unsigned long active;
     unsigned long count;
+    unsigned long min_lifetime;
+    unsigned long max_lifetime;
+    unsigned long trail_lifetime;
+    float start_size;
+    RGB color1;
+    RGB color2;
     struct ParticleSystem* next;
+    float (*remap)(float);
 } ParticleSystem;
 
 long long last_time;
@@ -107,7 +120,11 @@ void draw_oct(VectorFloat position, float radius, Vector screen_size, RGB color)
     glEnd();
 }
 
-char update_particle(ParticleSystem* system, Particle* current, Vector dimensions, float deltatime) {
+void render_trail(Trail** data, unsigned long width, unsigned long existence, unsigned long now, 
+            VectorFloat alignment, float rotation, Vector dimension, RGB color);
+
+char update_particle(ParticleSystem* system, Particle* current, unsigned long now, Vector dimensions, float deltatime) {
+    if (current->die_at < now) return 1;
     current->position.x += sinf(current->direction) * current->speed * deltatime;
     current->position.y += cosf(current->direction) * current->speed * deltatime;
     current->linear_speed.y -= 200.0f * deltatime;
@@ -115,14 +132,24 @@ char update_particle(ParticleSystem* system, Particle* current, Vector dimension
     current->position.y += current->linear_speed.y * deltatime;
     if (current->position.x <= -dimensions.x || current->position.x >= dimensions.x) return 1;
     if (current->position.y <= -dimensions.y || current->position.y >= dimensions.y) return 1;
-    draw_oct(current->position, 5, dimensions, (RGB) {255, 255, 255});
+    RGB color = lerp_color(system->color1, system->color2, 1 - ((current->die_at - now) / (float) (system->max_lifetime)));
+    float size;
+    if (system->remap) {
+        size = system->start_size * system->remap(((current->die_at - now) / (float) (system->max_lifetime)));
+    }
+    else {
+        size = system->start_size * ((current->die_at - now) / (float) (system->max_lifetime));
+    }
+    render_trail(&current->trail, size, system->trail_lifetime, now, current->position, current->direction + M_PI_2, dimensions, color);
+    draw_oct(current->position, size, dimensions, color);
     return 0;
 }
 
-void update_system(ParticleSystem* system, Vector dimensions, float deltatime) {
+void update_system(ParticleSystem* system, Vector dimensions, unsigned long now, float deltatime) {
     for (unsigned long i = 0; i < system->count; i++) {
         if (system->current[i].active) {
-            if (update_particle(system, system->current + i, dimensions, deltatime)) {
+            if (update_particle(system, system->current + i, now, dimensions, deltatime)) {
+                // free trail
                 system->current[i].active = false;
                 system->active--;
             }
@@ -144,6 +171,7 @@ void __render_trail(Trail* trail, unsigned long width, unsigned long existence, 
         trail->position.x - p.x * width * factor,
         trail->position.y - p.y * width * factor
     }, dimension);
+    glColor3ub(trail->color.r, trail->color.g, trail->color.b);
     glVertex2f(c1.x, c1.y);
     glVertex2f(c2.x, c2.y);
     glVertex2f(c2.x, c2.y);
@@ -152,12 +180,13 @@ void __render_trail(Trail* trail, unsigned long width, unsigned long existence, 
 }
 
 void render_trail(Trail** data, unsigned long width, unsigned long existence, unsigned long now, 
-            VectorFloat alignment, Vector dimension, RGB color) {
+            VectorFloat alignment, float rotation, Vector dimension, RGB color) {
     Trail* new_trail = malloc(sizeof(Trail));
     new_trail->next = *data;
     new_trail->since = now;
     new_trail->position = alignment;
-    new_trail->angle = mouse_rot;
+    new_trail->angle = rotation;
+    new_trail->color = color;
     *data = new_trail;
     
     Trail* trail = new_trail;
@@ -181,8 +210,8 @@ void render_trail(Trail** data, unsigned long width, unsigned long existence, un
         glColor3ub(color.r, color.g, color.b);
         glBegin(GL_QUADS);
         VectorFloat c = point((VectorFloat) {
-            mouse_pos.x,
-            mouse_pos.y
+            alignment.x,
+            alignment.y
         }, dimension);
         glVertex2f(c.x, c.y);
         glVertex2f(c.x, c.y);
@@ -191,7 +220,33 @@ void render_trail(Trail** data, unsigned long width, unsigned long existence, un
     }
 }
 
-Trail* trail_item = NULL;
+void init_particle(ParticleSystem* particle_system, int x, int y, int amount, unsigned long min_lifetime, 
+        unsigned long max_lifetime, unsigned long trail_lifetime, RGB color1, RGB color2, float linear_speed, 
+        float random_speed, float size, float (*remap)(float)) {
+    particle_system->current = malloc(sizeof(Particle) * amount);
+    particle_system->count = amount;
+    particle_system->active = amount;
+    particle_system->min_lifetime = min_lifetime;
+    particle_system->max_lifetime = max_lifetime;
+    particle_system->color1 = color1;
+    particle_system->color2 = color2;
+    particle_system->next = NULL;
+    particle_system->start_size = size;
+    particle_system->trail_lifetime = trail_lifetime;
+    particle_system->remap = remap;
+    for (unsigned long i = 0; i < particle_system->count; i++) {
+        particle_system->current[i].position.x = x;
+        particle_system->current[i].position.y = y;
+        particle_system->current[i].die_at = last_time + particle_system->min_lifetime +
+                ((float) random() / (float) RAND_MAX) * (particle_system->max_lifetime - particle_system->min_lifetime);
+        particle_system->current[i].linear_speed.x = 0;
+        particle_system->current[i].linear_speed.y = 0;
+        particle_system->current[i].direction = ((float) random() / (float) RAND_MAX) * 2 * M_PI;
+        particle_system->current[i].speed = ((float) random() / (float) RAND_MAX) * random_speed + linear_speed;
+        particle_system->current[i].active = true;
+        particle_system->current[i].trail = NULL;
+    }
+}
 
 void display() {
     long long now = __time_get_current_timestamp_internal();
@@ -205,12 +260,21 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     ParticleSystem* cursor = first;
+    ParticleSystem** prev = &first;
     while (cursor) {
-        //update_system(cursor, dimensions, deltatime);
-        cursor = cursor->next;
+        update_system(cursor, dimensions, now, deltatime);
+        if (cursor->active) {
+            prev = &cursor->next;
+            cursor = cursor->next;
+        }
+        else {
+            ParticleSystem* next = cursor->next;
+            *prev = next;
+            free(cursor->current);
+            free(cursor);
+            cursor = next;
+        }
     }
-    //draw_oct(mouse_pos, 10, dimensions, (RGB) {255, 255, 255});
-    render_trail(&trail_item, 10, 1000000, now, mouse_pos, dimensions, (RGB) {255, 255, 255});
 
     glFlush();
 }
@@ -230,6 +294,21 @@ void mouse(int x, int y) {
     mouse_pos.y = new.y;
 }
 
+float remap(float in) {
+    return sinf(in * M_PI);
+}
+
+void mouse_event(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        ParticleSystem* new_system = malloc(sizeof(ParticleSystem));
+        init_particle(new_system, x, glutGet(GLUT_WINDOW_HEIGHT) - y, 
+                100, 200000, 500000, 100000, (RGB) {255, 255, 10},
+                (RGB) {255, 0, 0}, 100, 900, 5, NULL);
+        new_system->next = first;
+        first = new_system;
+    }
+}
+
 ParticleSystem particle_system;
 
 int main(int argc, char** argv) {
@@ -243,7 +322,7 @@ int main(int argc, char** argv) {
         size = (Vector) {glutGet(GLUT_SCREEN_HEIGHT), glutGet(GLUT_SCREEN_HEIGHT)};
     #endif
     glutInitWindowSize(size.x, size.y);
-    glutInitWindowPosition(100, 100);
+    glutInitWindowPosition(0, 0);
     glutCreateWindow("");
 
     glutDisplayFunc(&display);
@@ -251,28 +330,14 @@ int main(int argc, char** argv) {
     glutKeyboardFunc(&keyboard);
     glutPassiveMotionFunc(&mouse);
     glutMotionFunc(&mouse);
+    glutMouseFunc(&mouse_event);
     glDisable(GL_LIGHTING);
     init_oct_points();
     glutFullScreen();
 
     last_time = __time_get_current_timestamp_internal();
 
-    particle_system = (ParticleSystem) {
-        malloc(sizeof(Particle) * 100),
-        100,
-        100,
-        NULL
-    };
-    for (unsigned long i = 0; i < particle_system.count; i++) {
-        particle_system.current[i].position.x = 0;
-        particle_system.current[i].position.y = 0;
-        particle_system.current[i].linear_speed.x = 0;
-        particle_system.current[i].linear_speed.y = 0;
-        particle_system.current[i].direction = ((float) random() / (float) RAND_MAX) * 2 * M_PI;
-        particle_system.current[i].speed = ((float) random() / (float) RAND_MAX) * 100.0f + 200.0f;
-        particle_system.current[i].active = true;
-    }
-    first = &particle_system;
+    first = NULL;
 
     glutMainLoop();
     return 0;
